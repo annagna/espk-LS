a5142a21842bd9f57b848146a9a4c7b991067911
annagna <annagna08@gmail.com>
2026-03-30 09:31:10 -0300

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,16 @@
+// let nome = prompt("Digite seu nome:");
+
+// alert("Bem-vindo, " + nome + "!");
+
+// console.log('Rodando no Console!');
+// console.log(`Nome que o usuário inseriu: ${nome}`);
+
+// let idade = parseInt(prompt('Digite sua idade:'));
+// alert(`Legal, você terá ${idade + 25} daqui à 25 anos!`);
+
+let numero = parseInt(prompt('Digite um número:'));
+let outronumero = parseInt(prompt('Digite outro número:'));
+
+console.log(`A soma dos números é: ${(numero) + (outronumero)}`);
+
+alert(`A multiplicação dos números é: ${parseInt(numero) * parseInt(outronumero)}`);
